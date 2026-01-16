656f6a0b64ad9d591233ea878b019a6a7c4db5f1
Nischal Shrestha <sthanischal5060@gmail.com>
2026-01-16 20:47:34 +0545

    full structured folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import Navbar from './components/layout/Navbar'
+import Hero from './components/layout/Hero'
+import Body1 from './components/layout/Body1'
+import Body2 from './components/layout/Body2'
+import Body3 from './components/layout/Body3'
+import Footer from './components/layout/Footer'
+import imafgs from "./assets/images/UI.png"
 const App = () => {
   return (
     <div>
+      <img src={imafgs} alt="" />
+      <Navbar/>
+      <Hero />
+      <Body1 />
+      <Body2 />
+      <Body3 />
+      <Footer />
       
     </div>
   )
