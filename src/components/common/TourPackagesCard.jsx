22306bd64911bb924018bb6e6518808b55eb13a2
Nischal Shrestha <sthanischal5060@gmail.com>
2026-01-16 21:58:01 +0545

    footer added

---
+++ src/components/common/TourPackagesCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TourPackagesCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TourPackagesCard
--- src/components/layout/Body3.jsx
+++ src/components/layout/Body3.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Body3 = () => {
   return (
-    <div>
+    <div className='h-160 w-full bg-gray-700 flex items-center px-14 gap-2 '>
+      <div className='bg-gray-600 w-1/2 h-140 rounded-2xl'>
+      <div>
+        <h3>Tour Packages</h3>
+      <p>Affordable customizable</p>
+      </div>
+      </div>
+      <div className=' bg-gray-400 w-1/2 h-140'></div>
       
     </div>
   )
