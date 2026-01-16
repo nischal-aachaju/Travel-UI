1c0996f81c2c267b3bfee98fe655f70c6454e85b
Nischal Shrestha <sthanischal5060@gmail.com>
2026-01-16 20:33:19 +0545

    folder structure

---
+++ src/components/layout/Body2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Body2 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Body2
---
+++ src/components/layout/Body3.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Body3 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Body3
