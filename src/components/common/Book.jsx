16d631f53b0d909de915b6740fd78109a76854a0
Nischal Shrestha <sthanischal5060@gmail.com>
2026-01-17 21:38:02 +0545

    fixing images

--- src/components/common/Book.jsx
+++ src/components/common/Book.jsx
@@ -2,7 +2,7 @@
 
 const Book = ({e}) => {
   return (
-    <div className='h-1/3 rounded-2xl shadow-[0px_0px_6px_rgba(17,17,26,0.3)] w-full px-2 py-2 bg-[#a3c9bc] flex hover:shadow-[0px_0px_7px_rgba(17,17,26,0.5)] duration-200'>
+    <div className='h-1/3 rounded-2xl  shadow-[0px_0px_6px_rgba(17,17,26,0.3)] w-full px-2 py-2 bg-[#a3c9bc] flex hover:shadow-[0px_0px_7px_rgba(17,17,26,0.5)] duration-200'>
       <div className='h-full w-2.5/10 bg-gray-200 rounded-2xl  flex justify-center items-center'>{e.img}</div>
       <div className='h-full w-7.5/10  text-white px-4 py-3 flex flex-col justify-between'>
       <h4 className=' text-2xl font-semibold '>{e.title}</h4>
